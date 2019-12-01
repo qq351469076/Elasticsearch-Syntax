a350e92393256c7f41d65943bb859f9cb1f5d231
孔祥旭 <d90159@163.com>
2019-12-01 23:11:45 +0800

    v0.5

--- read.py
+++ read.py
@@ -298,6 +298,188 @@ def get_index_settings():
     pp(resp)
 
 
+def single_agg():
+    """
+    按照目的地进行Bucket统计
+    """
+    data = {
+        # 做聚合不应返回结果, 所以此处应该是0
+        "size": 0,
+        # 聚合函数
+        "aggs": {
+            # 自定义聚合之后的名字
+            "flight_dest": {
+                # term不会做分词
+                # terms将input作为一个整体, 不进行任何分词, 而Index会对每个文档做索引, 索引的词和input匹配不上
+                # 所以有可能会匹配不到, 需要对字段.keyword进行精确匹配, 如本次的DestCountry.keyword
+                "terms": {
+                    # 对DestCountry字段进行分桶聚合
+                    "field": "DestCountry"
+                }
+            }
+        }
+    }
+    resp = get(base_url + '/kibana_sample_data_flights/_search', headers=headers, json=data).json()
+    pp(resp)
+
+
+def many_agg():
+    """
+    多字段聚合
+    """
+    data = {
+        "size": 0,
+        "aggs": {
+            "flight_dest": {
+                "terms": {
+                    "field": "DestCountry"
+                },
+                "aggs": {
+                    "avg_price": {
+                        # Metric方法, 求平均
+                        "avg": {
+                            "field": "AvgTicketPrice"
+                        }
+                    },
+                    "max_price": {
+                        # 求最大
+                        "max": {
+                            "field": "AvgTicketPrice"
+                        }
+                    },
+                    "min_price": {
+                        # 求最小
+                        "min": {
+                            "field": "AvgTicketPrice"
+                        }
+                    }
+                }
+            }
+        }
+    }
+    resp = get(base_url + '/kibana_sample_data_flights/_search', headers=headers, json=data).json()
+    pp(resp)
+
+
+def agg_and_status():
+    data = {
+        "size": 0,
+        "aggs": {
+            "flight_dest": {
+                "terms": {
+                    "field": "DestCountry"
+                },
+                "aggs": {
+                    "start_price": {
+                        # stats方法
+                        "stats": {
+                            # 对AvgTicketPrice字段进行count, sum, avg, max, min
+                            "field": "AvgTicketPrice"
+                        }
+                    },
+                    "weather": {
+                        "terms": {
+                            # 对DestWeather字段聚合, 比如大雪天有多少天, 雾天有多少天
+                            "field": "DestWeather",
+                            # 聚合后的结果显示多少条, 如果有6条, 只显示前5条
+                            "size": 5
+                        }
+                    }
+                }
+            }
+        }
+    }
+    resp = get(base_url + '/kibana_sample_data_flights/_search', headers=headers, json=data).json()
+    pp(resp)
+
+
+def difficult_query():
+    """
+    将Query转成Filter, 忽略TF-IDF计算, 避免相关性算分的开销
+    Filter可以有效利用缓存
+    """
+    data = {
+        "explain": 'true',
+        "query": {
+            "constant_score": {
+                "filter": {
+                    # Term是表达语意的最小单位, 搜索和利用统计语言语言模型进行自然语言处理都需要处理Tem
+                    # 在ES中, Term查询, 对input不做分词, 会将input作为一个整体, 在倒排索引中查找准确的词项,
+                    # 并且使用相关度算分公式为每个包含该词项的文档进行相关度算分 - 例如"App Store"
+                    # 通过constant_score将查询转成一个Filtering, 避免算分, 并利用缓存, 提高性能
+                    # term要查询的字段, 如果是多值字段, 也会出现在结果里, 因为term的查询是包含, 而不是等于
+                    "term": {
+                        # 词 必须要 精准 才能准确命中, 同时对字段.keyword进行精确取值
+                        "keyword.keyword": "糖"
+                    }
+                }
+            }
+        }
+    }
+    resp = get(base_url + '/keywords/_search', headers=headers, json=data).json()
+    pp(resp)
+
+
+def range_query():
+    data = {
+        "query": {
+            "constant_score": {
+                "filter": {
+                    # 数字范围查询, 不进行打分
+                    "range": {
+                        "searchavg7days": {
+                            "gte": 1000,
+                            "lte": 2000
+                        }
+                    }
+                }
+            }
+        }
+    }
+    resp = get(base_url + '/keywords/_search', headers=headers, json=data).json()
+    pp(resp)
+
+
+def date_query():
+    data = {
+        "query": {
+            "constant_score": {
+                "filter": {
+                    # 数字范围查询, 不进行打分
+                    "range": {
+                        # 对date字段进行日期查询
+                        "date": {
+                            # 时间必须大于一年以前
+                            # y 年      M 月        d 天
+                            # w 周      H/h 小时     m 分钟     s 秒
+                            "gte": 'now-1y'
+                        }
+                    }
+                }
+            }
+        }
+    }
+    resp = get(base_url + '/keywords/_search', headers=headers, json=data).json()
+    pp(resp)
+
+
+def exist_query():
+    data = {
+        "query": {
+            "constant_score": {
+                "filter": {
+                    "exists": {
+                        # 查询包含keyword字段的文档
+                        "field": "keyword"
+                    }
+                }
+            }
+        }
+    }
+    resp = get(base_url + '/keywords/_search', headers=headers, json=data).json()
+    pp(resp)
+
+
 if __name__ == "__main__":
     headers = {
         "Content-Type": "application/json"
@@ -338,11 +520,19 @@ if __name__ == "__main__":
     # es_count_limit()  # 分页
     # es_filter()  # 返回指定字段, 过滤没用字段
 
-    # es_select()  # 指定一个字段查询, 字段=值
-    # es_select_and()  # 指定一个字段查询, 两个字段查询一个结果, 同上面的另外一种写法
-    # es_use_fileds_selcet()  # 指定一个字段查询, 同上上的另外一种写法
-    # simple_query_string_query()  # 简单查询语法, 用得少
-    # es_use_many_fileds_selcet()  # 指定多个字段查询, 查询相同的结果
-    # find_many_field()  # 查询不同字段的不同值
+    # single_agg()  # 单字段桶聚合
+    # many_agg()  # 多字段聚合
+    # agg_and_status()  # 求agg和status方法
+
+    # es_select()  # match查询, 指定一个字段查询, 字段=值
+    # es_select_and()  # match查询, 指定一个字段查询, 两个字段查询一个结果, 同上面的另外一种写法
+    # es_use_fileds_selcet()  # match查询, 指定一个字段查询, 同上上的另外一种写法
+    # simple_query_string_query()  # match查询, 简单查询语法, 用得少
+    # es_use_many_fileds_selcet()  # match查询, 指定多个字段查询, 查询相同的结果
+    # find_many_field()  # match查询, 查询不同字段的不同值
+    difficult_query()  # term查询, 复合型查询 Constant Score转为Filter, 去掉相关性算分
+    # range_query()  # 数值范围查询, 不进行打分
+    # date_query()  # 日期范围查询, 不进行打分
+    # exist_query()  # 查询包含某字段的文档, 同时不进行打分
 
     # es_phrase_match()  # 短语搜索, 词必须是按照先后顺序出现, 中间允许有几个字符串介入
